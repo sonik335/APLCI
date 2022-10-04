f803f1b71642a19d455cb222694757c3ab9d8b22
Kirill <sonik3000.83@gmail.com>
2022-10-05 00:13:30 +0300

    initial commit

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -18,8 +18,9 @@ class MobileBankApiTestV1 {
                 .then()
                 .statusCode(200)
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-                .body("[0].currency", equalTo("RUB"))
+                .body("[0].currency", equalTo("RUR"))
                 .body("[1].currency",equalTo("USD"));
+
 
 
     }
